1e3d004d5701e7ce3acfb1ceb009faa26f69bbf2
MuPattarakorn <pattarakorn.ssng@gmail.com>
2023-02-17 21:32:53 +0700

    Create fetchWeather.jsx

---
+++ src/api/fetchWeather.jsx
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const url = 'https://api.openweathermap.org/data/2.5/weather';
+const key = '63a5023941139a34dbe4b31a6aa10fb8';
+
+const fetchWeather = async (query) => {
+    const { data } = await axios.get(url, {
+        params: {
+            q: query,
+            units: 'metric',
+            APPID: key
+        }
+    });
+    return data;
+}
+
+export default fetchWeather
